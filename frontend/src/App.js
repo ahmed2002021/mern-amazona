20d619656bd9095d07c9dcdaa60074b29c8b5317
ahmed <ahmedelsayed2002021@gmail.com>
2022-10-02 23:53:20 +0200

    mern-amazon-step1

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="#">Amazon</a>
+        <main>List items</main>
+      </header>
+    </div>
+  );
+}
+
+export default App;
